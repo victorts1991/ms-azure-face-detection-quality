760594e19765d6ad17c0ac26108a8c3bf6e6b78a
Victor Toupitzen Specian <victorts1991@gmail.com>
2026-03-05 18:22:54 -0300

    update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "rg-terraform-state"
-    storage_account_name = "stfacequalitytfe7dff1"
+    storage_account_name = "stfacequalitytf212f99"
     container_name       = "tfstate"
     key                  = "face-quality.terraform.tfstate"
   }
