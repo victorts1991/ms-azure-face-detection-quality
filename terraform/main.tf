2aea41cdab6f8c15d7146eab6eff9c78888fca44
Victor Toupitzen Specian <victorts1991@gmail.com>
2026-03-06 15:13:34 -0300

    commit test

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "rg-terraform-state"
-    storage_account_name = "stfacequalitytf31de52"
+    storage_account_name = "stfacequalitytf271d3e"
     container_name       = "tfstate"
     key                  = "face-quality.terraform.tfstate"
   }
